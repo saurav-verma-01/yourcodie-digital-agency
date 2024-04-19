cd311de284bc213d4ce795c037db50709c7c4c10
Saurav kumar verma <sauravkumarverma22@gmail.com>
2024-04-19 21:09:36 +0530

    website drafft temlate

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,25 @@ const toggleNavBar = () => {
 // Adding events on NavToggle Btn and Overlay
 
 addEventOnElement([navToggleBtn, overlay], "click", toggleNavBar);
+
+// Parallax effect
+
+const parallaxElements = document.querySelectorAll("[data-parallax]");
+
+window.addEventListener("mousemove", (event) => {
+  for (let i = 0, len = parallaxElements.length; i < len; i++) {
+    const movementX =
+      (event.clientX / window.innerWidth) *
+      Number(parallaxElements[i].dataset.parallaxSpeed);
+    const movementY =
+      (event.clientY / window.innerHeight) *
+      Number(parallaxElements[i].dataset.parallaxSpeed);
+
+    parallaxElements[i].animate(
+      {
+        transform: `translate(${movementX}px, ${movementY}px)`,
+      },
+      { duration: 500, fill: "forwards" }
+    );
+  }
+});
